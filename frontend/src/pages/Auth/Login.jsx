57e88e40e0b22b49251619c309d46ee56624c744
Giusus94 <87708249+Giusus94@users.noreply.github.com>
2025-04-25 18:15:32 +0100

    Fix corretto auth.js

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { checkAuth } from './utils/auth';
+import { checkAuth } from '../../utils/auth';
+
 
 const App = () => {
   const navigate = useNavigate();
