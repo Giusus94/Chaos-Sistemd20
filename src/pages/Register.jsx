850bca2afd467471dbdcaa960e2aa2139e991ce9
Giusus94 <87708249+Giusus94@users.noreply.github.com>
2025-05-01 23:30:53 +0100

    stringfy corretto

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -25,10 +25,9 @@ const Register = () => {
 
     try {
       const res = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
-
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userData),
+        body: JSON.stringify({email,password, nickname,avatar }),
       });
 
       const data = await res.json();
