ad76219e9e2a3ffa03cf2506e5bbd80567a32ea5
Giusus94 <87708249+Giusus94@users.noreply.github.com>
2025-05-02 00:48:36 +0100

    updat 0.0.023

--- api/register.js
+++ api/register.js
@@ -1,4 +1,4 @@
-const { connectToDatabase } = require("../../lib/mongodb");
+const { connectToDatabase } = require("../lib/mongodb");
 
 module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
